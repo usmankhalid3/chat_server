faa9d6344528d1b60642cc1a0da4feda20c2597d
Usman Khalid <usman.khalid@gamevstudios.com>
2013-12-22 12:18:12 +0500

    Fixing script

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,7 +2,7 @@ PROC_SEARCH='[s]erver.Main'
 IS_AWAKE=`ps -ef | grep $PROC_SEARCH | wc -l`
 if [ $IS_AWAKE -ge 1 ]; then
 	echo "Killing server.."
-	#kill `ps -ef | grep $PROC_SEARCH | awk '{ print $2 }'`
+	kill `ps -ef | grep $PROC_SEARCH | awk '{ print $2 }'`
 	exit 0;
 else
 	echo "No server instance found"
